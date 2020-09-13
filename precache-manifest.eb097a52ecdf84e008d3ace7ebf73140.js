6a8445c214aed459e55aed50fc8b7ef155399603
wjzards <wjzardsopeerapong@gmail.com>
2020-09-13 16:51:05 +0700

    Updates

---
+++ precache-manifest.eb097a52ecdf84e008d3ace7ebf73140.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d3a1c374214818c15d58b54de1cc40",
+    "url": "/portfolio-me/index.html"
+  },
+  {
+    "revision": "8105abe87a47e2ccd58f",
+    "url": "/portfolio-me/static/js/2.adb9c370.chunk.js"
+  },
+  {
+    "revision": "ccf491244da6a3591a7f18b2d9dc8874",
+    "url": "/portfolio-me/static/js/2.adb9c370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e653eb2e6544f6bea81b",
+    "url": "/portfolio-me/static/js/main.084b3dfe.chunk.js"
+  },
+  {
+    "revision": "1c43eea61547356219c0",
+    "url": "/portfolio-me/static/js/runtime-main.e3e4c93e.js"
+  },
+  {
+    "revision": "31ef508bc9ae4ed6884f386d5c05e5d5",
+    "url": "/portfolio-me/static/media/about.31ef508b.jpg"
+  },
+  {
+    "revision": "9b4a987577a6aa71b475e0828317bf05",
+    "url": "/portfolio-me/static/media/design1.9b4a9875.png"
+  },
+  {
+    "revision": "40dfd354970992845fd8cd4dee7663ed",
+    "url": "/portfolio-me/static/media/design2.40dfd354.png"
+  },
+  {
+    "revision": "b31fe8789fe5db413ba01f6e4664020a",
+    "url": "/portfolio-me/static/media/design3.b31fe878.png"
+  },
+  {
+    "revision": "10b9b50b3c051f3ebb7d7fac68345a90",
+    "url": "/portfolio-me/static/media/design4.10b9b50b.png"
+  },
+  {
+    "revision": "bea46982943b74348c272e779604e56b",
+    "url": "/portfolio-me/static/media/design5.bea46982.png"
+  },
+  {
+    "revision": "4d2b623b7c8f321c5339074450cb1418",
+    "url": "/portfolio-me/static/media/developer1.4d2b623b.png"
+  },
+  {
+    "revision": "c0eff14e5623e2b2f71b19967c65878c",
+    "url": "/portfolio-me/static/media/developer2.c0eff14e.png"
+  },
+  {
+    "revision": "9d03cbfbde31310cd9c9562eceab5d3e",
+    "url": "/portfolio-me/static/media/logo.9d03cbfb.png"
+  },
+  {
+    "revision": "4031e4d2740f8218715ea3a8d791e8a3",
+    "url": "/portfolio-me/static/media/logotwo.4031e4d2.png"
+  },
+  {
+    "revision": "1e3412c71fd79f424ccbd332e4968b6c",
+    "url": "/portfolio-me/static/media/profile.1e3412c7.jpg"
+  }
+]);
